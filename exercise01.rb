f8f12204a451e53af2d22845335d06f11d73d589
David Ma <davidma415@gmail.com>
2018-07-24 23:36:21 -0400

    Finished exercise 2

--- exercise01.rb
+++ exercise01.rb
@@ -8,5 +8,17 @@ def odd_nums(arr)
   return counter
 end
 
+def includes_name(arr, name)
+  arr.map!(&:downcase)
+  if arr.include? name.downcase
+    puts "Hello there #{name.capitalize}"
+  else
+    puts "Who goes there?!"
+  end
+end
+
 arr1 = odd_nums([3, 2, 3, 4, 6, 7, 13]) # 26
 puts arr1
+
+names = ['Aaron', 'Ron', 'Vinnie']
+includes_name(names, 'rob')
